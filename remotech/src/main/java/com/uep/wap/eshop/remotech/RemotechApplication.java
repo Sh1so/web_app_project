bd37631b05a963a3b280e1a22014a92415d24415
Igor Lewicki <129624981+Sh1so@users.noreply.github.com>
2025-03-29 16:00:48 +0100

    initialization of spring project

---
+++ remotech/src/main/java/com/uep/wap/eshop/remotech/RemotechApplication.java
@@ -0,0 +1,13 @@
+package com.uep.wap.eshop.remotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotechApplication.class, args);
+	}
+
+}
---
+++ remotech/src/test/java/com/uep/wap/eshop/remotech/RemotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uep.wap.eshop.remotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
